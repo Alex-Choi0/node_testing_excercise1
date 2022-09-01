254552a0cd2238156f9bc3e13fa69b4d0854b7a2
alex <choijeaho86@gmail.com>
2022-09-01 20:33:17 +0900

    Error Test

--- src/app/Main.ts
+++ src/app/Main.ts
@@ -3,6 +3,7 @@ import { parse, UrlWithParsedQuery } from "url";
 
 export class Fun{
     public static parseUrl(url : string) : UrlWithParsedQuery {
+        if(!url || url.length <=0) throw Error('url is empty') // url이 비어있을시 에러발생
         return parse(url, true);
     }
     public static toUpperC(arg : string){
--- src/test/Main.test.ts
+++ src/test/Main.test.ts
@@ -34,5 +34,25 @@ describe('Main test suite', () => {
 
     // 추가 테스트 케이스 작업에 대한 메모
     test.todo("추가되는 코드에 대한 테스트 케이스 작성!!!")
+
+    test('test invalid URL', () => {
+        // expectError함수로 감싸서 에러를 확인한다
+        // function expectError(){
+        //     Fun.parseUrl('')
+        // }
+        expect(() => {// Arrow Function을 이용하여 expect안에 실행해서 에러를 검출할수 있다.
+            Fun.parseUrl('')
+        }).toThrowError('url is empty')
+    })
+
+    // try, catch문을 이용하거 에러를 검출하고 검증한다.
+    test('test invalid URL with try and catch', () => {
+        try{
+            Fun.parseUrl('');
+        } catch (err){
+            expect(err).toBeInstanceOf(Error)
+            expect(err).toHaveProperty('message', 'url is empty')
+        }
+    })
 })
 
